081a02580b23c5b1d8e05863e266b16d3852cfb5
Sascha Rezagholinia <sascha.rezagholinia@sennder.com>
2021-10-09 17:00:52 +0200

    add

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -18,7 +18,7 @@ class LessonInline(admin.StackedInline):
 
 # Register your models here.
 class CourseAdmin(admin.ModelAdmin):
-    inlines = [LessonInline]
+    inlines = [LessonInline, QuestionInLine]
     list_display = ('name', 'pub_date')
     list_filter = ['pub_date']
     search_fields = ['name', 'description']
@@ -27,6 +27,10 @@ class CourseAdmin(admin.ModelAdmin):
 class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInLine]
+    list_display = ['text']
+
 
 # <HINT> Register Question and Choice models here
 
@@ -34,3 +38,5 @@ admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
